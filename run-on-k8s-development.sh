306556c49846233e9907b240b2df0d503f52dee4
Shuichi Machida <shuichi.machida@gmail.com>
2022-07-22 17:19:06 +0900

    first commit

---
+++ run-on-k8s-development.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+  echo "引数に start もしくは stop を指定してください"
+  exit 1
+fi
+
+if [ $1 = "start" ]; then
+
+  echo "Deploying microservice-demo services on k8s development env..."
+  kubectl apply -k services/overlays/development
+
+  echo "Waiting for the services to start up..."
+  kubectl wait --timeout=600s --for=condition=ready pod --all
+fi
+
+if [ $1 = "stop" ]; then
+  echo "Deleting microservice-demo services  on k8s development env..."
+  kubectl delete -k services/overlays/development
+fi
